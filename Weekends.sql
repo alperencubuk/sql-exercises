c5fc193d1ba2a63b004a2f6d57ae704699516ce6
alperencubuk <alperencubuk@outlook.com>
2022-09-14 16:37:56 +0300

    Weekends

--- Weekends.sql
+++ Weekends.sql
@@ -1,43 +1,43 @@
 -- CREATE OR REPLACE Procedure AddWeekends(start_day IN CHAR(8), end_day IN CHAR(8)) IS
 
 DECLARE
-	start_day CHAR(8) := '20220101';
-	end_day CHAR(8) := '20240101';
-	---------------------------------------------------
-	start_date DATE := TO_DATE(start_day,'YYYYMMDD');
-	end_date DATE := TO_DATE(end_day,'YYYYMMDD');
-	char_date CHAR(8);
-	today NUMBER; -- 6 = CUMARTESI, 7 = PAZAR
-	day_exist NUMBER;
+    start_day CHAR(8) := '20220101';
+    end_day CHAR(8) := '20240101';
+    ---------------------------------------------------
+    start_date DATE := TO_DATE(start_day,'YYYYMMDD');
+    end_date DATE := TO_DATE(end_day,'YYYYMMDD');
+    char_date CHAR(8);
+    today NUMBER; -- 6 = CUMARTESI, 7 = PAZAR
+    day_exist NUMBER;
 
 BEGIN
-	WHILE (start_date <= end_date)
-	LOOP
-	    today := TO_CHAR(start_date, 'D');
-		-- for English should use 1 = SUNDAY, 7 = SATURDAY
-	    IF (today = 6 OR today = 7) THEN
-	        char_date := TO_CHAR(start_date, 'YYYYMMDD');
+    WHILE (start_date <= end_date)
+    LOOP
+        today := TO_CHAR(start_date, 'D');
+        -- for English should use 1 = SUNDAY, 7 = SATURDAY
+        IF (today = 6 OR today = 7) THEN
+            char_date := TO_CHAR(start_date, 'YYYYMMDD');
 
-	        SELECT COUNT(*) INTO day_exist
-	        FROM MY_TABLE_NAME
-	        WHERE HOLIDAY_DATE = char_date;
+            SELECT COUNT(*) INTO day_exist
+            FROM MY_TABLE_NAME
+            WHERE HOLIDAY_DATE = char_date;
 
-	        IF day_exist = 0 THEN
-	            INSERT INTO MY_TABLE_NAME
-	            (HOLIDAY_DATE, OFFICIAL_HOLIDAY)
-	            VALUES (char_date, 1);
-	        END IF;
+            IF day_exist = 0 THEN
+                INSERT INTO MY_TABLE_NAME
+                (HOLIDAY_DATE, OFFICIAL_HOLIDAY)
+                VALUES (char_date, 1);
+            END IF;
 
-	    END IF;
+        END IF;
 
-		-- for English should use today = 1
-		IF today = 7 THEN
-			start_date := start_date + 6;
-		ELSE
-			start_date := start_date + 1;
-		END IF;
+        -- for English should use today = 1
+        IF today = 7 THEN
+            start_date := start_date + 6;
+        ELSE
+            start_date := start_date + 1;
+        END IF;
 
-	END LOOP;
+    END LOOP;
 END;
 
 /* CREATE TABLE
